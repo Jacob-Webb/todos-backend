3f4ab810f012825e22e11a0a8532716133b5bee3
Jacob Webb <jacob@jacob-webb.com>
2020-08-02 06:43:51 -0700

    added user lookup to acquire roles

---
+++ restful-web-services/src/main/java/com/jacobwebb/restfulwebservices/service/UserDetailServiceImpl.java
@@ -0,0 +1,41 @@
+package com.jacobwebb.restfulwebservices.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jacobwebb.restfulwebservices.dao.UserJpaRepository;
+import com.jacobwebb.restfulwebservices.model.User;
+
+@Service
+@Transactional
+public class UserDetailServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UserJpaRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username).orElse(null);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		authorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+		
+		return new org.springframework.security.core.userdetails.User(
+					user.getUsername(), 
+					user.getPassword(), 
+					authorities);
+	}
+}
