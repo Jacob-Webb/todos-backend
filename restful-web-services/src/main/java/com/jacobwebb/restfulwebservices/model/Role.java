5e87b62544789a614d1a605c48ac0758be6a42e9
Jacob Webb <jacob@jacob-webb.com>
2020-08-08 21:36:26 -0700

    implemented getters, setters, toString, etc for User, Role, Privilege

--- restful-web-services/src/main/java/com/jacobwebb/restfulwebservices/model/Role.java
+++ restful-web-services/src/main/java/com/jacobwebb/restfulwebservices/model/Role.java
@@ -2,6 +2,7 @@ package com.jacobwebb.restfulwebservices.model;
 
 import java.util.Collection;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +18,9 @@ public class Role {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
 	
+	@Column(name = "name")
 	private String name;
+	
 	@ManyToMany(mappedBy = "roles")
 	private Collection<User> users;
 	
@@ -29,4 +32,97 @@ public class Role {
 		inverseJoinColumns = @JoinColumn(
 			name = "privilege_id", referencedColumnName = "id"))
 	private Collection<Privilege> privileges;
+	
+	public Role() {
+		
+	}
+
+	public Role(Long id, String name, Collection<User> users, Collection<Privilege> privileges) {
+		this.id = id;
+		this.name = name;
+		this.users = users;
+		this.privileges = privileges;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Collection<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Collection<User> users) {
+		this.users = users;
+	}
+
+	public Collection<Privilege> getPrivileges() {
+		return privileges;
+	}
+
+	public void setPrivileges(Collection<Privilege> privileges) {
+		this.privileges = privileges;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", users=" + users + ", privileges=" + privileges + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((privileges == null) ? 0 : privileges.hashCode());
+		result = prime * result + ((users == null) ? 0 : users.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (privileges == null) {
+			if (other.privileges != null)
+				return false;
+		} else if (!privileges.equals(other.privileges))
+			return false;
+		if (users == null) {
+			if (other.users != null)
+				return false;
+		} else if (!users.equals(other.users))
+			return false;
+		return true;
+	}
+	
+	
 }
