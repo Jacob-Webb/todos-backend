80b7bb5965039951f4955867947939e1246895ff
Jacob Webb <jacob@jacob-webb.com>
2020-09-21 22:30:46 -0700

    added button for sidenav

--- frontend/todo/src/app/menu/menu.component.ts
+++ frontend/todo/src/app/menu/menu.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
@@ -7,30 +6,11 @@ import { BasicAuthenticationService } from '../service/basic-authentication.serv
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
+export class MenuComponent {
 
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef,
-              media: MediaMatcher,
-              public basicAuthenticationService: BasicAuthenticationService
-              ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-}
-
-
-  /*
   constructor(public basicAuthenticationService: BasicAuthenticationService) { }
 
   ngOnInit(): void {}
 
 }
-*/
+
