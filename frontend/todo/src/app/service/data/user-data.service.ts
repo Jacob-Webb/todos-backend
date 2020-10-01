543b9ecc318f425d4a939e74be1889c4735e908d
Jacob Webb <jacob@jacob-webb.com>
2020-10-01 06:03:36 -0700

    created user http calls

--- frontend/todo/src/app/service/data/user-data.service.ts
+++ frontend/todo/src/app/service/data/user-data.service.ts
@@ -8,44 +8,26 @@ import { API_URL } from 'src/app/app.constants';
 })
 export class UserDataService {
 
-  constructor() { }
-}
-
-
-
-/*
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Todo } from '../../list-todos/list-todos.component';
-import { API_URL } from 'src/app/app.constants';
-
-
-@Injectable({
-  providedIn: 'root'
-})
-export class TodoDataService {
-
   constructor(private http: HttpClient) { }
 
-  retrieveAllTodos(username) {
-    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`)
+  retrieveAllUsers() {
+    return this.http.get<User[]>(`${API_URL}/users`)
   }
 
-  deleteTodo(username, id) {
-    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`)
+  deleteUser(id) {
+    return this.http.delete(`${API_URL}/users/${id}`)
   }
 
-  retrieveTodo(username, id) {
-    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
+  retrieveUser(id) {
+    return this.http.get<User>(`${API_URL}/users/${id}`)
   }
 
-  updateTodo(username, id, todo) {
-    return this.http.put(`${API_URL}/users/${username}/todos/${id}`, todo);
+  updateUser(id, user) {
+    return this .http.put(`${API_URL}/users/${id}`, user)
   }
 
-  createTodo(username, todo) {
-    return this.http.post(`${API_URL}/users/${username}/todos`, todo);
+  createUser(user) {
+    return this.http.post(`${API_URL}/users`, user);
   }
 
 }
-*/
