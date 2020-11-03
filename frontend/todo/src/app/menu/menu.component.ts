39aa8429994efed1d7556571704cd372dccd9f24
Jacob Webb <jacob@jacob-webb.com>
2020-11-03 05:57:37 -0800

    added account submenu

--- frontend/todo/src/app/menu/menu.component.ts
+++ frontend/todo/src/app/menu/menu.component.ts
@@ -8,9 +8,13 @@ import { AUTHENTICATED_USER, BasicAuthenticationService } from '../service/basic
 })
 export class MenuComponent {
   userName: string
+  role: string;
+  name: string;
 
   constructor(public basicAuthenticationService: BasicAuthenticationService) {
-    this.userName = localStorage.getItem(AUTHENTICATED_USER)
+    this.userName = localStorage.getItem(AUTHENTICATED_USER);
+    this.name = localStorage.getItem('firstName') + ' ' + localStorage.getItem('lastName');
+    this.role = localStorage.getItem('role');
   }
 
   ngOnInit(): void {}
