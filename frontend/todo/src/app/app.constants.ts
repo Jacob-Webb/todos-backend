46e6007a977ddeda579f5783981a3db25ed2cd03
Jacob Webb <jacob@jacob-webb.com>
2020-11-08 06:36:07 -0800

    remove constants

--- frontend/todo/src/app/app.constants.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = "http://localhost:8080/api"
-export const BASE_URL = "http://localhost:8080"
-export const REGISTER_URL = "http://localhost:8080/user/register"
-export const RESET_PASSWORD_URL = "http://localhost:8080/user/resetPassword"
-export const SIGNIN_TOKEN = "dummy"
