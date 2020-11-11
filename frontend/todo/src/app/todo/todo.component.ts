3e7d86ed5b4cca916b82495b6fac71be1ae596bd
Jacob Webb <jacob@jacob-webb.com>
2020-11-11 12:11:16 -0800

    todo list restyled for material

--- frontend/todo/src/app/todo/todo.component.ts
+++ frontend/todo/src/app/todo/todo.component.ts
@@ -3,6 +3,8 @@ import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BasicAuthenticationService } from '../service/basic-authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
 
 @Component({
   selector: 'app-todo',
@@ -13,44 +15,68 @@ export class TodoComponent implements OnInit {
 
   id:number;
   todo: Todo;
+  createTodo: boolean = false;
+  todoForm: FormGroup;
 
   constructor(
     private todoService: TodoDataService,
     private route: ActivatedRoute,
     private router: Router,
-    private basicAuthenticationService: BasicAuthenticationService
-  ) { }
+    private basicAuthenticationService: BasicAuthenticationService,
+    fb: FormBuilder
+  ) {
+    this.todoForm = fb.group({
+      'title': [''],
+      'description': [''],
+      'date': ['', Validators.compose([Validators.required])],
+      'done': ['']
+    })
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(this.id, '', false, new Date())
+    this.todo = new Todo(this.id, '', '', false, new Date())
+    this.createTodo = true;
 
     if (this.id != -1) {
-      this.todoService.retrieveTodo(this.basicAuthenticationService.getAuthenticatedUser(), this.id)
+      this.createTodo = false;
+      this.todoService.retrieveTodo(this.id, this.basicAuthenticationService.getAuthenticatedUser())
       .subscribe (
-        data => this.todo = data
+        data => {
+          this.todo = data;
+          this.todoForm.controls['title'].setValue(this.todo.title);
+          this.todoForm.controls['description'].setValue(this.todo.description);
+          this.todoForm.controls['date'].setValue(this.todo.targetDate);
+          this.todoForm.controls['done'].setValue(this.todo.done);
+        }
       )
     }
   }
 
-  saveTodo() {
+  onSubmit() {
+    this.todo.title = this.todoForm.controls['title'].value;
+    this.todo.description = this.todoForm.controls['description'].value
+    this.todo.targetDate = this.todoForm.controls['date'].value
     if (this.id == -1) {
       // Create Todo
+      this.todo.done = false;
       this.todoService.createTodo(this.basicAuthenticationService.getAuthenticatedUser(), this.todo).subscribe(
         data => {
-          console.log(data)
 
         }
       )
       this.router.navigate(['todos']);
     } else {
-      this.todoService.updateTodo(this.basicAuthenticationService.getAuthenticatedUser(), this.id, this.todo).subscribe(
+      this.todo.done = this.todoForm.controls['done'].value;
+      this.todoService.updateTodo(this.id, this.basicAuthenticationService.getAuthenticatedUser(), this.todo).subscribe(
         data => {
-          console.log(data);
         }
-      )
-      this.router.navigate(['todos']);
+      ),
+      this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['todos']);
+      });
     }
+
   }
 
 }
